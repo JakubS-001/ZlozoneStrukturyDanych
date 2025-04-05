93a367b24f2b4c9797a6f3cdf90751743d5ab9b8
Kirnero <filipzyw@gmail.com>
2025-04-05 22:02:04 +0200

    Brain actually brains

--- commands/brain.py
+++ commands/brain.py
@@ -1,17 +1,24 @@
 #decision making to activate different commands
 
-def commandcenter(command): #Process the command
-    if command == "help":
-        Help()
+def commandcenter(command):
+
+    known_commands = { # all commands are lowercase
+        "help": Info_dump
+        # Add more commands here
+    }
+    
+    if command in known_commands:
+        known_commands[command]()
     else:
-        print("Command not recognized. Type 'Help' for available commands.")
+        print("Unknown command. Type 'help' for a list of available commands.")
 
 
-def Help():
+def Info_dump():
     print("\nAvailable commands:")
     print("1. Help - Show available commands")
     print("2. Exit - Exit the program")
-    
     # Add more commands as needed
+    print("\nAll commands are case-insensitive. For example, 'help' and 'Help' are the same command.")
+    # Some more miscellaneous information for the user
 
     print()
