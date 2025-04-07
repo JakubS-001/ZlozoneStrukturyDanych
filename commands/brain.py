d0af04ea113e63efbf248b2db5bb2ff7ffaccbd0
Kirnero <filipzyw@gmail.com>
2025-04-07 16:28:23 +0200

    min max returns nodes

--- commands/brain.py
+++ commands/brain.py
@@ -40,15 +40,15 @@ def find_min(node):
     current_node = node
     while current_node.left:
         current_node = current_node.left
-    return current_node.value
+    return current_node
 
 def find_max(node):
     current_node = node
     while current_node.right:
         current_node = current_node.right
-    return current_node.value
+    return current_node
 
 def find_min_max(node):
-    print(f"Min: {find_min(node)}")
-    print(f"Max: {find_max(node)}")
+    print(f"Min: {find_min(node).value}")
+    print(f"Max: {find_max(node).value}")
 
