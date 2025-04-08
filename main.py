49414b3ea0cc5b7a5e7fb26de41d771adf769da5
JakubS-001 <jakubstasierski@gmail.com>
2025-04-08 15:42:17 +0200

    renamed2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 from commands.brain import commandcenter
 from commands.tree_creation import create_tree
-from commands.child_genocide import mass_removal, remove_all
+from commands.removing_children import mass_removal, remove_all
 
 def main():
     # Command-line arguments: python main.py --tree <tree type(AVL/BST)>
