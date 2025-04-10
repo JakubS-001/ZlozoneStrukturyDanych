80a27840889649471ebd03a220fcd0efbc2f57e0
Kirnero <filipzyw@gmail.com>
2025-04-10 19:24:45 +0200

    Komendy bez spacji

--- commands/brain.py
+++ commands/brain.py
@@ -9,14 +9,17 @@ from commands.export_tik import export_print_tree_tikz
 
 def commandcenter(command, root):
     global known_commands
+    command = command.replace(" ", "")
     known_commands = { # all commands are lowercase
         "help": [info_dump, "Show available commands"],
         "exit": [None, "Exit the program"],
         "print": [None, "Print the tree based on given arguments:\n\tall - prints the elements in tree in in-order, post-order and pre-order\n\ttree - prints tree structure\n\t....-order - prints the elements in tree in the respective order (look argument all)"],
         "findminmax" : [find_min_max, "Print the minimum and maximum tree value"],
         "delete": [mass_removal, "Delete given amount of nodes from the tree"],
-        "delete all": [initiate_remove_all, "Delete all nodes from the tree in post-order"],
-        "create tree": [create_tree_from_terminal, "Create a new tree from beggining"],
+        "deleteall": [initiate_remove_all, "Delete all nodes from the tree in post-order"],
+        "remove": [mass_removal, "Works the same as Delete"],
+        "removeall": [initiate_remove_all, "Works the same as Delete all"],
+        "createtree": [create_tree_from_terminal, "Create a new tree from beggining"],
         "dsw": [dsw_balance, "Balance tree using DSW"],
         "export": [export_print_tree_tikz, "Export tree to tickzpicture (Latex)"]
         # Add more commands here
@@ -24,7 +27,7 @@ def commandcenter(command, root):
     
     
     if command[0:5]=="print":
-        if command[6:]: print_brain(command[6:], root)
+        if command[5:]: print_brain(command[5:], root)
         else: print_brain("all", root)
     elif command in known_commands:
         function = known_commands[command][0]
